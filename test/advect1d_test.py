ff8d30125feb75d66c306b7ca09cf58521cf085f
John Haiducek <jhaiduce@gmail.com>
2025-01-21 15:29:51 -0500

    Adjust inputs to reduce test execution times

--- test/advect1d_test.py
+++ test/advect1d_test.py
@@ -2,8 +2,8 @@ from advect1d import cdaweb
 from datetime import datetime
 
 def test_get_cdf():
-    cdaweb.get_cdf('sp_phys','OMNI2_H0_MRG1HR',datetime(2005,1,1),datetime(2005,2,1),['KP1800'])
+    cdaweb.get_cdf('sp_phys','OMNI2_H0_MRG1HR',datetime(2005,1,1),datetime(2005,1,2),['KP1800'])
 
 def test_fetch_and_advect():
     from advect1d.advect_imf import fetch_and_advect
-    fetch_and_advect(datetime(2017, 9, 6, 20),datetime(2017, 9, 7, 5))
+    fetch_and_advect(datetime(2017, 9, 6, 20),datetime(2017, 9, 6, 21))
